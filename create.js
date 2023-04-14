c45cb28debcb2ada748dd5cdafb26e2a0c66b525
Karen Bonganay <kbonganay.intern.integratr@outlook.com>
2023-04-14 10:56:44 +0800

    initial commit

---
+++ create.js
@@ -0,0 +1,8 @@
+function myFunction() {
+   
+    const Container = document.querySelector('.Container');
+    const input = document.createElement('input');
+    input.setAttribute("value", "text");
+    Container.appendChild(input);
+    
+    }   
